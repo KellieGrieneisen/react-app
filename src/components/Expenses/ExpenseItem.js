2cc921cf8ccdafc7b7a3ac1c6813923e6c5dc471
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-09-20 19:01:50 -0400

    using event handlers to change/update a form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
-import Card from './UI/Card.js'
+import Card from '../UI/Card.js'
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
